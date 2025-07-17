3db8f3a38781d1f8fc385a9e0dab85be84718f48
Yosef Hershberg <berioo2000@gmail.com>
2025-07-17 12:39:20 +0300

    db

--- dal/playerDal.js
+++ dal/playerDal.js
@@ -2,14 +2,14 @@ import { mongoClient } from '../lib/mongo.js';
 import { ObjectId } from 'mongodb';
 
 export async function createPlayer(playerData) {
-    const db = mongoClient.db();
+    const db = mongoClient.db("myDB");
     const collection = db.collection('players');
     const result = await collection.insertOne(playerData);
     return result.insertedId;
 }
 
 export async function deletePlayer(playerId) {
-    const db = mongoClient.db();
+    const db = mongoClient.db("myDB");
     const collection = db.collection('players');
     const result = await collection.deleteOne({ _id: new ObjectId(playerId) });
     return result.deletedCount === 1;
